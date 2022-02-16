1f17c024d4209e5d4e28d97cbb3dbbf9e64d4546
Or Abudi <orabudi@gmail.com>
2022-02-16 15:27:59 +0200

    added routes for apis

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,89 @@
 const express = require("express");
+const winston = require("winston");
+const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 const port = 3000; // default port to listen
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = () => {
+  const env = process.env.NODE_ENV || "development";
+  const isDevelopment = env === "development";
+  return isDevelopment ? "debug" : "warn";
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "white",
+};
+
+winston.addColors(colors);
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf((info) => `${info.timestamp} ${info.message}`)
+);
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: "logs/error.log",
+    level: "error",
+  }),
+  new winston.transports.File({ filename: "logs/all.log" }),
+];
+
+const Logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+});
+
+const stream = {
+  write: (message) => Logger.http(message),
+};
+
+const skip = () => {
+  const env = process.env.NODE_ENV || "development";
+  return env !== "development";
+};
+
+const logMiddleware = morgan(
+  ":method :url :status :res[content-length] - :response-time ms",
+  { stream, skip }
+);
+
+app.use(cors());
+app.use(logMiddleware);
+
+// app routes
+// create new user
+app.post("/users/register", (req, res) => {});
+
+// get user
+app.get("/users", (req, res) => {});
+
+// get all regestries
+app.get("/regestry/all", (req, res) => {});
+
+//get certain registry
+app.get("/registry", (req, res) => {});
+
+// empty certain registry
+app.put("/registry", (req, res) => {});
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
@@ -9,5 +92,5 @@ app.get("/", (req, res) => {
 
 // start the Express server
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+  return Logger.info(`Express is listening at http://localhost:${port}`);
 });
